b1416e03f96db972090b925e31b58065983ae37e
amaank8797 <amaank8797@gmail.com>
2024-07-11 17:07:14 +0530

    Initial Commit

---
+++ SourceControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SourceControl
+//
+//  Created by Amaan Amaan on 11/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Amaan Amaan on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
